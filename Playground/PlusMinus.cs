e00c32a1b9b28d0e5a33a8700134a6aefa82d476
dom <dom@pitula.pl>
2023-10-17 19:40:46 +0200

    Add project files.

---
+++ Playground/PlusMinus.cs
@@ -0,0 +1,40 @@
+﻿class PlusMinus
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        decimal positiveCount = 0;
+        decimal negativeCount = 0;
+        decimal neutralCount = 0;
+
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (arr[i] > 0) { positiveCount++; }
+            else if (arr[i] < 0) { negativeCount++; }
+            else { neutralCount++; }
+        }
+
+        Console.WriteLine((positiveCount / arr.Count).ToString("N6"));
+        Console.WriteLine((negativeCount / arr.Count).ToString("N6"));
+        Console.WriteLine((neutralCount / arr.Count).ToString("N6"));
+
+    }
+
+}
+
+/*class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> arr = new List<int> { -4, 3, -9, 0, 4, 1 };
+
+        Result.plusMinus(arr);
+    }
+}*/
